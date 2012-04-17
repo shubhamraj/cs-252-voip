9b5d8a227fd7a76e1ffe0db472545f4af199c28d
farrukh.y@gmail.com <farrukh.y@gmail.com@a56f70e5-8517-6db6-9a07-bbe0633a015d>
2012-04-17 22:49:14 +0000

    Basic set of classes

---
+++ trunk/CS252_voip/src/purdue/cs252/voip/CS252_voipActivity.java
@@ -0,0 +1,13 @@
+package purdue.cs252.voip;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CS252_voipActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+    }
+}
