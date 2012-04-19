ba46ff85b13ea942e9578017a54902b83fb3e0f8
10chandlerc@gmail.com <10chandlerc@gmail.com@a56f70e5-8517-6db6-9a07-bbe0633a015d>
2012-04-19 21:58:53 +0000

    VoicePlayerServer.java

--- trunk/CS252_voip/src/purdue/cs252/voip/VoicePlayerServer.java
+++ trunk/CS252_voip/src/purdue/cs252/voip/VoicePlayerServer.java
@@ -1,5 +1,91 @@
 package purdue.cs252.voip;
 
-public class VoicePlayerServer {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
+public class VoicePlayerServer{
+	private AudioTrack player;
+	private int sampleRate = 11025;
+	private int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+	private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+	private int bufferSize;
+	private byte[] buffer;
+	private String ipAddress;
+	private int portNumber;
+	private InetAddress serverAddr;
+	private DatagramSocket socket;
+	private boolean running = false;
+	
+	public VoicePlayerServer(String ip, int port){
+		bufferSize = AudioTrack.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+		buffer = new byte[bufferSize];
+		ipAddress = ip;
+		portNumber = port;
+	}
+	
+	public void stopRunning(){
+		running = false;
+	}
+
+	public void startRunning(){
+		running = true;
+		// Start the server
+		new Thread(new VoiceServer()).start();
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) { }
+		
+		new Thread(new VoicePlayer()).start();
+	}
+	
+	private class VoicePlayer implements Runnable{
+		public VoicePlayer(){
+			player = new AudioTrack(AudioManager.STREAM_VOICE_CALL, sampleRate, 
+					channelConfig, audioFormat, bufferSize, AudioTrack.MODE_STREAM);
+		}
+		
+		public void run() {
+			player.play();
+
+			// Loop forever playing the audio
+			while (running) {
+				// Play the sound
+				player.write(buffer, 0, bufferSize);
+			}
+			
+		}
+	}
+	
+	private class VoiceServer implements Runnable{
+		public VoiceServer(){
+			try{
+				serverAddr = InetAddress.getByName(ipAddress);
+				socket = new DatagramSocket(portNumber, serverAddr);
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void run() {
+			try {
+				while(running){
+					DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+					socket.receive(packet);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
 }
