2c5e5696bab92423eed77a28d37046a39371a071
farrukh.y@gmail.com <farrukh.y@gmail.com@a56f70e5-8517-6db6-9a07-bbe0633a015d>
2012-04-19 20:05:03 +0000

    Voice Capture Client and sound recorder is implemented.

---
+++ CS252_voip/src/purdue/cs252/voip/Recorder.java
@@ -0,0 +1,53 @@
+/**
+ * Recorder.java
+ * 
+ * Continuously records sound to buffer.
+ * buffer is shared with VoiceCaptureClient class.
+ * 
+ * @author Farrukh Yakubov
+ * @date 4/19/2012
+ * 
+ */
+
+package purdue.cs252.voip;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+public class Recorder implements Runnable {
+
+	int sampleRate;
+	int channelConfig;
+	int audioFormat;
+	int bufferSize;
+	byte buffer[];
+	
+	public Recorder(byte buffer[]){
+		
+		sampleRate = 11025;
+		channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+		audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+		bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+		this.buffer = buffer;
+	}
+	
+	private AudioRecord recorder;
+
+	@Override
+	public void run() {
+		// Create a new recorder
+		recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate,
+				channelConfig, audioFormat, bufferSize);
+
+		// Start the recording
+		recorder.startRecording();
+
+		// Loop forever recording input
+		while (true) {
+			// Read from the microphone
+			recorder.read(buffer, 0, bufferSize);
+		}
+
+	}
+}
--- CS252_voip/src/purdue/cs252/voip/VoiceCaptureClient.java
+++ CS252_voip/src/purdue/cs252/voip/VoiceCaptureClient.java
@@ -1,5 +1,63 @@
+/**
+ * VoiceCaptureClient.java
+ * 
+ * Continuously sends sound data packets to VoicePlayerServer on connected device.
+ * 
+ * buffer is shared with Recorder class, which writes sound data to the buffer.
+ * 
+ * @author Farrukh Yakubov
+ * @date 4/19/2012
+ * 
+ */
+
 package purdue.cs252.voip;
 
-public class VoiceCaptureClient {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import android.util.Log;
 
+public class VoiceCaptureClient implements Runnable {
+	
+	String SERVERIP;
+	int SERVERPORT;
+	
+	int bufferSize;
+	byte[] buffer;
+
+	Thread rec;
+	
+	public VoiceCaptureClient(final String SERVERIP, final int SERVERPORT){
+		this.SERVERIP = SERVERIP;
+		this.SERVERPORT = SERVERPORT;
+		
+		// Temporary buffer to store the audio input. Used to record and play.
+		buffer = new byte[bufferSize];
+		
+		rec = new Thread(new Recorder(buffer));
+		rec.start();
+	}
+	
+	@Override
+	public void run() {
+		try {
+			// Retrieve the ServerName
+			InetAddress serverAddr = InetAddress.getByName(SERVERIP);
+
+			Log.d("UDP", "C: Connecting...");
+			/* Create new UDP-Socket */
+			DatagramSocket socket = new DatagramSocket();
+			
+			while (true) {
+				/* Create UDP-packet with
+				 * data & destination(url+port) */
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddr, SERVERPORT);
+				/* Send out the packet */
+				socket.send(packet);
+			}
+			
+		} catch (Exception e) {
+			Log.e("UDP", "C: Error", e);
+		}
+	}
 }
