fb70e4ed621857a50ba4b3cc31c7b111303724bb
farrukh.y@gmail.com <farrukh.y@gmail.com@a56f70e5-8517-6db6-9a07-bbe0633a015d>
2012-04-24 23:46:24 +0000

    test

--- CS252_voip/src/purdue/cs252/voip/MainActivity.java
+++ CS252_voip/src/purdue/cs252/voip/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
