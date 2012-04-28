5a9311444858cc21e5afddb1e947a7e26d9baedc
farrukh.y@gmail.com <farrukh.y@gmail.com@a56f70e5-8517-6db6-9a07-bbe0633a015d>
2012-04-28 21:15:35 +0000

    recorder

---
+++ trunk/CS252_voip/src/purdue/cs252/voip/Recorder.java
@@ -0,0 +1,50 @@
+/**
+ * Recorder.java
+ * 
+ * Continuously records sound to buffer.
+ * buffer is shared with VoiceCaptureClient class.
+ * 
+ * @author Farrukh Yakubov
+ * @date 4/19/2012
+ * 
+ */
+
+package purdue.cs252.voip;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+public class Recorder implements Runnable {
+
+	private AudioRecord recorder;
+	int sampleRate = 8000;
+	int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+	int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+	int bufferSize;
+	static byte buffer[];
+	
+	public Recorder(){
+		bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+		buffer = new byte[bufferSize];
+	}
+	
+	
+//comment
+	@Override
+	public void run() {
+		android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+		// Create a new recorder
+		recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate,
+				channelConfig, audioFormat, bufferSize);
+
+		// Start the recording
+		recorder.startRecording();
+		// Loop forever recording input
+		while (VoiceCaptureClient.running) {
+			// Read from the microphone
+			recorder.read(buffer, 0, bufferSize);
+		}
+
+	}
+}
